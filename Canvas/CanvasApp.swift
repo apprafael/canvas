9d10c8a488591afb5c5a12bef519f017480da47f
Rafael Almeida <apprafael.almeida@gmail.com>
2025-03-27 14:42:43 -0300

    initial project

---
+++ Canvas/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Canvas
+//
+//  Created by Rafael Almeida on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
