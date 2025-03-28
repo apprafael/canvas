aca788fc88e3287d838ce13caf581b4729f5db92
Rafael Almeida <apprafael.almeida@gmail.com>
2025-03-28 12:50:27 -0300

    Make the pinch zoom the image

--- Canvas/Modules/Canvas/CanvasImageView.swift
+++ Canvas/Modules/Canvas/CanvasImageView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CanvasImageView: View {
     @State var selectedImage: Image
     @State var position: CGPoint = CGPoint(x: 100, y: 100)
+    @State private var scale: CGFloat = 1.0
 
     var body: some View {
         selectedImage
@@ -20,6 +21,13 @@ struct CanvasImageView: View {
                 DragGesture()
                     .onChanged { value in
                         position = value.location
+                    }
+            )
+            .scaleEffect(scale)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        scale = value
                     }
             )
     }
